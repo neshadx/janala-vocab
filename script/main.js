d8c7def598de0fde239bb8a557acdda809c48560
neshadx <neshadimagify@gmail.com>
2025-04-07 19:37:32 +0600

    added some code

--- script/main.js
+++ script/main.js
@@ -97,3 +97,64 @@ const displayLessonDetails = (wordDetails) => {
             </div>
   `;
 };
+
+
+const displayLesson = (data) => {
+  const lessonContainer = document.getElementById("lesson-details");
+  lessonContainer.innerHTML = "";
+  if (data.length == 0) {
+    lessonContainer.innerHTML = `
+      <div class="grid grid-cols-1 col-span-full">
+                    <div class="flex justify-center items-center flex-col gap-5 bg-base-200 py-14">
+                        <div><img class="h-28 w-28"
+                                src="images/alert-error.png" alt=""></div>
+                        <p class="text-sm font-semibold">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+                        <h1 class="text-3xl font-bold">নেক্সট Lesson এ যান</h1>
+                    </div>
+                </div>
+    `;
+    hideLoader();
+    return;
+  }
+  data.forEach((element) => {
+    const lessonCard = document.createElement("div");
+    lessonCard.innerHTML = `
+     <div class="card  bg-base-100 card-xl p-5  shadow-lg ">
+            <div>
+                <div class="border-2 border-blue-100 rounded-lg  bg-white card-body h-[280px]">
+                    <div class="flex gap-5 flex-col justify-center items-center">
+                        <h2 class="card-title text-3xl font-bold">${
+                          element.word
+                        }</h2>
+                        <p class="text-sm font-semibold">Meaning /Pronounciation</p>
+                        <h2 class="text-lg font-bold"><span class="font-bold text-lg">"</span> ${
+                          element.meaning
+                            ? `${element.meaning}`
+                            : `অর্থ নেই `
+                        } 
+                        <span>/</span> ${
+                          element.pronunciation
+                        } <span class="text-lg font-bold">"</span></h2>
+                    </div>
+                    <div class="flex justify-between gap-5 card-actions mt-5">
+                    <button onclick="loadLessonDetails(${
+                      element.id
+                    })" class="btn "><i class="fa-solid fa-circle-exclamation"></i></button>
+                        <button class="btn"><i class="fa-solid fa-volume-high"></i></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+     
+    `;
+    lessonContainer.append(lessonCard);
+  });
+  hideLoader();
+};
+/* Hide and show property */
+const makeHide = (id) => {
+  document.getElementById(id).style.display = "none";
+};
+const show = (id) => {
+  document.getElementById(id).style.display = "block";
+};
